284e9f5b8e047b26d51173cbce6a6b175a226f23
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-09 00:23:17 +0630

    feat(): todo endpoint finished

--- server/main.go
+++ server/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 type Todo struct {
 	ID    int    `json:"id"`
-	Title int    `json:"title"`
+	Title string `json:"title"`
 	Done  bool   `json:"done"`
 	Body  string `json:"body"`
 }
@@ -16,11 +17,40 @@ type Todo struct {
 func main() {
 	fmt.Println("Hello world")
 	app := fiber.New()
+	todos := []Todo{}
+	app.Post("/api/v1/todo", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+		return c.JSON(map[string]any{
+			"data": todo,
+		})
+	})
+	app.Get("/api/v1/todo", func(c *fiber.Ctx) error {
+		return c.JSON(map[string]any{
+			"data": todos,
+		})
+	})
+	app.Patch("/api/v1/todo/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil || id >= len(todos) || id < 0 {
+			return c.Status(400).JSON(map[string]string{"message": "invalid todo id provided"})
+		}
+		todos[id].Done = true
+		return c.JSON(map[string]Todo{
+			"data": todos[id],
+		})
+	})
+
 	app.Get("/health-check", func(c *fiber.Ctx) error {
 		c.JSON(map[string]string{
 			"message": "server is up and running",
 		})
 		return nil
 	})
+	app.Use(cors.Config{})
 	app.Listen(":8080")
 }
